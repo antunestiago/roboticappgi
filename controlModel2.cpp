151dcaeaba0f9ccf57a900c3dfbb7d8501b3c4fb
Renato Pacheco Vieira <renato.pacheco.r2@gmail.com>
2016-06-29 22:45:12 +0100

    Modelo de Controle Sordalane finalizado.

--- controlModel2.cpp
+++ controlModel2.cpp
@@ -10,8 +10,11 @@ double posx=0.0;
 double posy=0.0;
 double thetaO = 0.0;
 double pi = 3.14159;
-double gama1 = 0.4;
+double gama1 = 1;
 double k = 1;
+
+// posicao de parada (ou final ou desejada, como queira chamar)
+double Xp=0.5,Yp=-2, thetaP = -pi/2;
 
 void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg){
 	posx = msg->pose.pose.position.x;
@@ -21,20 +24,20 @@ void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg){
 
 double normRad(double angle){
     while(angle > pi)
-        return angle -= 2*pi;
+        return angle -= 2.0*pi;
     while(angle < -pi)
-        return angle += 2*pi;
+        return angle += 2.0*pi;
     return angle;
 }
 
 double thetaD(double x, double y){
     if(x==0.0)
         x = 0.1;
-    return (2*atan(y/x));
+    return (2.0*atan(y/x));
 }
 
 double funcSinC(double theta){
-    double sinc = 1;
+    double sinc = 1.0;
 
     if (abs(theta) > 0.001)
         sin(theta)/theta;
@@ -43,26 +46,36 @@ double funcSinC(double theta){
 }
 
 double funcSign(double x, double y){
-    if((x==0 && y<0) || x>0)
-        return 1;
-    else if((x=0 && y>0) || x<0)
-        return -1;
+    if((x==0.0 && y<0.0) || x>0.0)
+        return 1.0;
+    //else if((x==0.0 && y>=0.0) || x<0.0)
+    return -1.0;
 }
 
 double gama2(double gama, double a){
-    return gama/(1 + abs(a));
+    return gama/(1.0 + abs(a));
 }
 
 double coordCircular(double x, double y){
-    return funcSign(x,y)*sqrt(x*x + y*y)/(funcSinC(thetaD(x,y)/2));
+    return funcSign(x,y)*sqrt(x*x + y*y)/(funcSinC(thetaD(x,y)/2.0));
 }
 
-double funcB1(double x, double y, double theta){
-    return cos(theta)*(((x*thetaD(x,y))/y)-1) + sin(theta)*((thetaD(x,y)/2)*(1-((x*x)/(y*y))) + x/y); 
+double funcB1(double x, double y, double theta, double alpha){
+    double mybeta = y/x;
+    double theta_d = thetaD(x,y);
+
+    if (theta_d != 0)
+        return cos(theta)*((theta_d/mybeta) - 1.0) + sin(theta)*(theta_d/2.0*(1.0-1.0/pow(mybeta,2.0)) + 1.0/mybeta);
+    return cos(alpha);    
 }
 
 double funcB2(double x, double y, double theta){
-    return (cos(theta)*(2*(y/(x*x))*(1/(1+((y*y)/(x*x)))))) - (sin(theta)*2)/(x*(1+((y*y)/(x*x))));
+    double mybeta = y/x;
+    double theta_d = thetaD(x,y);
+
+    if (theta_d != 0)
+        return cos(theta)*(2.0*mybeta/((1.0+pow(mybeta,2.0))*x)) - sin(theta)*(2.0/((1.0+pow(mybeta,2.0))*x));
+    return sin(theta_d/2.0 - theta) * (2.0 / funcSinC(theta_d/2.0));
 }
 
 
@@ -77,30 +90,36 @@ int main(int argc, char **argv){
 	geometry_msgs::Twist vel_msg;
 	ros::Publisher velocity_publisher = n.advertise<geometry_msgs::Twist>("RosAria/cmd_vel",100);
 
-	double Ex=0.0,Ey=0.0; //Erro x e y
-	double Xp=1,Yp=2.0, thetaP = 0; //Posicao desejada de x e y em metros
-	double xM=0.0,yM=0.0, thetaM = 0.0; //orientacao da velocidade linear e rotacao
+	double Ex=0.0,Ey=0.0;
+	double xM=0.0,yM=0.0, thetaM = 0.0;
 	double u=0.0,w=0.0, alpha = 0.0;
-	double a=0;
+	double a=0.0, fb1, fb2;
 
 	while(ros::ok()){ //loop ate pressionar ctrl+C
 			//ROS_INFO("xpositionDentroDoWhil: [%f]",posx);	
 			//ROS_INFO("passouasdeeeWhile");
-		Ex = Xp - posx;
-		Ey = Yp - posy;
+		Ex = posx - Xp;
+		Ey = posy - Yp;
+		thetaM = thetaO - thetaP;
 
-		thetaM = normRad(thetaO - thetaP);
 		xM = (cos(thetaP)*Ex + sin(thetaP)*Ey);
 		yM = (-sin(thetaP)*Ex + cos(thetaP)*Ey);			
 
-		ROS_INFO("xM, yM, theta, theta_er, theta_d: [%f, %f, %f, %f]",xM,yM, thetaO, thetaM);	
-		//ROS_INFO("Erro de posicao: [%f, %f]",Ex,Ey);
 		a = coordCircular(xM,yM);	
 		alpha = normRad(thetaM-thetaD(xM,yM));
-		u = -gama2(gama1,a)*funcB1(xM,yM,thetaM)*a; //perguntar se o paramatro é theta erro ou theta atual
-		w = (-funcB2(xM,yM,thetaM)*u) - (k*alpha);
 
-		//ROS_INFO("u, w: [%f, %f]",u,w);	
+        fb1 = funcB1(xM,yM,thetaM, alpha);
+		u = -gama2(gama1,a)*fb1*a;
+
+        fb2 = funcB2(xM,yM,thetaM);
+		w = (-fb2*u) - (k*alpha);
+
+        /*
+		ROS_INFO("xM, yM, thetaM: [%f, %f, %f]",xM,yM, thetaM);	
+		ROS_INFO("thetaO, thetaP, alpha, : [%f, %f, %f]",thetaO, thetaP, alpha);	
+		ROS_INFO("fb1, fb2: [%f, %f]",fb1, fb2);	
+		ROS_INFO("a, alpha, u, w: [%f, %f,%f, %f]",a, alpha,u,w);	
+        */
 
 		//velocidades lineares
         //seta a velocidade liner no eixo x para a frente do robo
